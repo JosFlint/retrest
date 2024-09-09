f1b934ec78d20ef06abb60d17025735ec5786d42
jo Flint <Josflint@hotmail.com>
2024-09-09 08:52:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
         offScreenCtx.drawImage(video, 0, 0, offScreenCanvas.width, offScreenCanvas.height);
 
         // Apply brightness and contrast to the canvas
-        offScreenCtx.filter = 'sepia(1) saturate(2.5) hue-rotate(-70deg) brightness(0,9) contrast(1.7)'; // Adjusted filter values
+        offScreenCtx.filter = 'sepia(1) saturate(2.5) hue-rotate(-55deg) brightness(0,9) contrast(1.7)'; // Adjusted filter values
         offScreenCtx.drawImage(offScreenCanvas, 0, 0, offScreenCanvas.width, offScreenCanvas.height);
 
         // Add text watermark to the canvas
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Apply filter to live video feed
     function applyFilter() {
-        video.style.filter = 'sepia(1) saturate(2.5) hue-rotate(-70deg) brightness(0.9) contrast(1.7)'; // Adjusted filter values
+        video.style.filter = 'sepia(1) saturate(2.5) hue-rotate(-55deg) brightness(0.9) contrast(1.7)'; // Adjusted filter values
     }
 
     // Call applyFilter function on video play
